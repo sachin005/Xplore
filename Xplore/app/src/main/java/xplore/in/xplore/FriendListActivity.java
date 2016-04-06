9fcf7c1d34d8fae287d8d33ae65e86c36459c643
Sachin B. Chavan <sachin.chavan@student.csulb.edu>
2016-04-05 18:55:29 -0700

    Runs without errors but has access tokens warnings.

---
+++ Xplore/app/src/main/java/xplore/in/xplore/FriendListActivity.java
@@ -0,0 +1,103 @@
+package xplore.in.xplore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FriendListActivity extends AppCompatActivity {
+
+    private final String TAG = "friendsActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Log.d(TAG, "Started friendListActivity");
+
+//        List<String> friendsList = getFriendsList();
+
+//        if(friendsFromBundle == null) {
+//            friendsFromBundle = {{"Null reply"}};
+//        }
+
+        Intent intent = getIntent();
+        String[] friendsFromBundle = intent.getStringArrayExtra("friends_list");
+        List<String> friendsList = Arrays.asList(friendsFromBundle);
+        Log.d(TAG, "friendList length: " + friendsList.size());
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.friends_listview, friendsList);
+        ListView listView = (ListView) findViewById(R.id.friendslistView);
+
+        if(listView != null)
+            listView.setAdapter(arrayAdapter);
+
+//        listView.setOnClickListener();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    protected List<String> getFriendsList() {
+
+        final List<String> friendList = new ArrayList<>();
+        Log.d(TAG, "in getFriendsList");
+        // make API call
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/friends",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        JSONArray jsonArray = response.getJSONArray();
+                        Log.d(TAG, "jsonArray length = " + jsonArray.length());
+                        Log.d(TAG, "in onCompleted");
+
+                        try {
+                            if (jsonArray.length() == 0 || jsonArray.isNull(0)) {
+                                friendList.add("No friends use this app");
+                                Log.d(TAG, "jsonArray 0 or null");
+                            }
+                            else {
+                                for(int i = 0; i < jsonArray.length(); i++) {
+                                    friendList.add(jsonArray.getJSONObject(i).get("name").toString());
+                                }
+                                Log.d(TAG, "json -- for" + jsonArray.length());
+                            }
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                            Log.d(TAG, "some error in graphrequest.callback oncompleted");
+                        }
+                    }
+                }
+        );
+        return friendList;
+    }
+
+}
